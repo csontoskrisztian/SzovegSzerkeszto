1ec9bb0ab828a70e2b0154477b6bfadf4debb740
Csontos Krisztián <csontos.krisztian11@gmail.com>
2020-10-27 13:59:27 +0100

    Alapvető funkciók elkészítése

--- my.js
+++ my.js
@@ -1,46 +1,96 @@
-//Elemek kigyűjtése
-let buttonH1 = document.getElementById('buttonH1');
-let buttonP = document.getElementById('buttonP');
-let buttonStrong = document.getElementById('buttonStrong');
-let buttonRed = document.getElementById('buttonRed');
-let input = document.getElementById('input');
-let output = document.getElementById('output');
+// Változók
+let input = document.getElementById("input");
+let output = document.getElementById("output");
 
+
+
+// Lap újratöltésnél figyelmeztetés, hogy a módosítások elvesznek.
+window.onbeforeunload = function () {
+    return false;
+}
+
+// Output frissítése mikor változik az input
+document.getElementById("input").addEventListener("input", RenderOutput);
+
+// Kiírás Outputba az oldal megnyitásakor
 RenderOutput();
 
+
 //Események és eseménykezelők
-buttonH1.addEventListener('click', OnClickH1);
-buttonP.addEventListener('click', OnClickP);
-buttonStrong.addEventListener('click', OnClickStrong);
-buttonRed.addEventListener('click', OnClickRed)
-input.addEventListener('keyup', RenderOutput);
+// Gombok
+for (let i = 0; i < document.getElementsByClassName("ButtonModifier").length; i++) {
+    document.getElementsByClassName("ButtonModifier")[i].addEventListener('click', OnClickModifier);
+}
+// Betűszín inputok
+for (let i = 0; i < document.getElementsByClassName("CInput").length; i++) {
+    document.getElementsByClassName("CInput")[i].addEventListener('change', OnChangeColor);
+}
+// Háttér rádiógombok
+for (let i = 0; i < document.getElementsByClassName("BGCRadio").length; i++) {
+    document.getElementsByClassName("BGCRadio")[i].addEventListener('change', OnChangeBGColor);
+}
+// Lista
+for (let i = 0; i < document.getElementsByClassName("ButtonModifierList").length; i++) {
+    document.getElementsByClassName("ButtonModifierList")[i].addEventListener('click', OnClickModifierList);
+}
 
-function OnClickH1() {
-    let startTag = "<h1>"
-    let endTag = "</h1>"
-    
+
+
+// Az element id-je alapján megállítja mit használjon tagként
+function OnClickModifier() {
+    // console.log("Hello World")
+
+    let startTag = "<" + this.id + ">";
+    let endTag = "</" + this.id.split(" ")[0] + ">";
+    // console.log(startTag, endTag)
+
     Changer(startTag, endTag);
 }
 
-function OnClickP() {
-    let startTag = "<p>"
-    let endTag = "</p>"
-    
-    Changer(startTag, endTag);
+// Gomb id megváltoztatása az adott rgb kód alapján
+function OnChangeColor() {
+    let red = document.getElementById("Menu_Color_R").value;
+    let green = document.getElementById("Menu_Color_G").value;
+    let blue = document.getElementById("Menu_Color_B").value;
+
+    document.getElementsByClassName("Menu_Color_Button")[0].id = "span style='color: rgb(" + red + "," + green + "," + blue + ")'";
+    document.getElementsByClassName("Menu_Color_Button")[0].style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
 }
 
-function OnClickStrong() {
-    let startTag = "<Strong>";
-    let endTag = "</Strong>";
-    Changer(startTag, endTag);
+// Gomb id megváltoztatása az adott rádiógomb alapján
+function OnChangeBGColor() {
+    document.getElementsByClassName("Menu_Background_Color")[0].id = "span style='background-color: " + this.id + "'";
+    document.getElementsByClassName("Menu_Background_Color")[0].style.backgroundColor = this.id;
 }
 
-function OnClickRed() {
-    let startTag = "<span style='color: red;'>";
-    let endTag = "</span>";
-    Changer(startTag, endTag);
+// Lista beszúrása
+function OnClickModifierList() {
+    let startTag;
+    let endTag;
+
+    if (this.id == "ListUnordered") {
+        startTag = "<ul>";
+        endTag = "</ul>";
+    } else if (this.id == "ListOrdered") {
+        startTag = "<ol>";
+        endTag = "</ol>";
+    }
+
+
+    let text = input.value;
+    let start = input.selectionStart;
+    let end = input.selectionEnd;
+    let oldTag = "p";
+    let newTag = "li";
+
+    input.value = ChangeTagsInSelectedText(text, start, end, startTag, endTag, oldTag, newTag);
+
+    RenderOutput();
 }
 
+
+
+// Kiírás outputba
 function RenderOutput() {
     let html = input.value;
 
@@ -48,7 +98,7 @@ function RenderOutput() {
 }
 
 //Értékadó függények
-function Changer(startTag, endTag){
+function Changer(startTag, endTag) {
     let text = input.value;
     let start = input.selectionStart;
     let end = input.selectionEnd;
@@ -57,10 +107,21 @@ function Changer(startTag, endTag){
     RenderOutput();
 }
 
-function GetWrappedSelection(text, start, end, startTag, endTag){
+function GetWrappedSelection(text, start, end, startTag, endTag) {
     let left = text.slice(0, start);
     let middle = text.slice(start, end);
     let right = text.slice(end);
+
+    let newText = `${left}${startTag}${middle}${endTag}${right}`;
+    return newText;
+}
+
+function ChangeTagsInSelectedText(text, start, end, startTag, endTag, oldTag, newTag) {
+    let left = text.slice(0, start);
+    let middle = text.slice(start, end).replaceAll("<" + oldTag + ">", "<" + newTag + ">").replaceAll("</" + oldTag + ">", "</" + newTag + ">");
+    let right = text.slice(end);
+
+    console.log(middle);
 
     let newText = `${left}${startTag}${middle}${endTag}${right}`;
     return newText;
